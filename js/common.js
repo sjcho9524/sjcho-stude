1f23ac25cd312bca67517898c59e41eded8cd18d
sjcho9524 <sucho321@naver.com>
2022-03-24 21:24:06 +0900

    로그인 페이지 완성

--- js/common.js
+++ js/common.js
@@ -15,5 +15,6 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '');
 });
 
+
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear(); //2022
--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const badgeEl = document.querySelector('header .badges');
-const toTopEl = document.querySelector('#to-top');
 
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.screenY);
+  // 페이지 스크롤 위치가 500px이 넘으면.
   if (window.scrollY > 500) {
-    // 배지 숨기기
-    // gsap.to(요소, 지속시간, 옵션);
+    // Badge 요소 숨기기!
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
@@ -14,8 +15,10 @@ window.addEventListener('scroll', _.throttle(function () {
     gsap.to(toTopEl, .2, {
       x: 0
     })
+
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
   } else {
-    // 배지 보이기
+    // Badge 요소 보이기!
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
@@ -26,31 +29,38 @@ window.addEventListener('scroll', _.throttle(function () {
     })
   }
 }, 300))
-// _.throttle(함수, 시간)
-
+// 상단으로 스크롤 버튼을 클릭하면,
 toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
   gsap.to(window, .7, {
     scrollTo: 0
-  });
-});
+  })
+})
 
 
-const fadeEls = document.querySelectorAll('.visual .fade-in');
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
 fadeEls.forEach(function (fadeEl, index) {
-// gsap.to(요소, 지속시간, 옵션);
+  // 각 요소들을 순서대로(delay) 보여지게 함!
   gsap.to(fadeEl, 1, {
-    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7
+    delay: (index + 1) * .7,
     opacity: 1
-  });
-});
+  })
+})
 
 
-// new Swiper(선택자, 옵션)
+/**
+ * 슬라이드 요소 관리
+ */
 new Swiper('.notice-line .swiper-container', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
-});
+})
 new Swiper('.promotion .swiper-container', {
   // direction: 'horizontal', // 수평 슬라이드
   autoplay: { // 자동 재생 여부
@@ -133,11 +143,6 @@ floatingObject('.floating1', 1, 15)
 floatingObject('.floating2', .5, 15)
 floatingObject('.floating3', 1.5, 20)
 
-
-/**
- * 요소가 화면에 보여짐 여부에 따른 요소 관리
- */
-// 관리할 요소들 검색!
 const spyEls = document.querySelectorAll('section.scroll-spy')
 // 요소들 반복 처리!
 spyEls.forEach(function (spyEl) {
@@ -145,7 +150,7 @@ spyEls.forEach(function (spyEl) {
     .Scene({ // 감시할 장면(Scene)을 추가
       triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
       triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
-    });
+    })
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
-});
+})
